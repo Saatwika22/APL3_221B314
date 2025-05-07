4aa625b213346022949cc3174e0f3af3d1cdd3a1
221B314 <221B314@juet.co.in>
2025-05-07 11:31:30 +0530

    lab7

---
+++ lab 4/Rum.java
@@ -0,0 +1,10 @@
+class Rum extends Beverage{
+    private void serve(){
+         System.out.println("serve through waitor");
+    }
+public void templateMethod(int qty){
+pour(qty);
+    serve();
+}}
+
+
